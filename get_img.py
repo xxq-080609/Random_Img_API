17aca74655611018dcd806be2f68e0673e62657f
xxq-080609 <64231728+xxq-080609@users.noreply.github.com>
2022-10-17 13:18:01 -0700

    improved formatting

--- get_img.py
+++ get_img.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import sqlite3
+import sys
 import json
 import pydenticon
 from PIL import Image
@@ -10,11 +11,10 @@ from datetime import datetime
 database = sqlite3.connect("img_info.sqlite3")
 cursor = database.cursor()
 
-
 def get_acg():
     while True:
         try:
-            context = requests.get("https://api.lolicon.app/setu/v2?size=original&r18=1").text
+            context = requests.get("https://api.lolicon.app/setu/v2?size=original&size=regular&r18=1").text
             pid = json.loads(context)['data'][0]['pid']
             name = json.loads(context)['data'][0]['title']
             rt = os.system(f"""wget https://pixiv.cat/{pid}.jpg""")
@@ -36,8 +36,8 @@ def get_wallpaper():
             if rt == 0:
                 img = Image.open(f"""./wallpaper{num}.png""")
                 img_x, img_y = img.size
-                cursor.execute("""INSERT INTO img VALUES (?, ?, ?, ?, ?, ?)"""
-                               ("""wallpaper{num}""", 'wallpaper', 'png', f"""img/wallpaper{num}.png""", img_x, img_y)
+                cursor.execute("""INSERT INTO img VALUES (?, ?, ?, ?, ?, ?)""",
+                               (f"""wallpaper{num}""", 'wallpaper', 'png', f"""img/wallpaper{num}.png""", img_x, img_y))
                 database.commit()
         except KeyboardInterrupt:
             break
@@ -45,15 +45,16 @@ def get_wallpaper():
 
 def get_avatar():
     while True:
-        num = int(round(datetime.now().timestamp() * 10000))
+        num = 0;
+        num += 1;
         try:
             generator = pydenticon.Generator(10, 10)
             avatar = generator.generate(str(num), 240, 240)
-            avatarwrite = open(f"""./avatar{num}.png""")
+            avatarwrite = open(f"""avatar{num}.png""", "wb")
             avatarwrite.write(avatar)
             avatarwrite.close()
-            cursor.execute("""INSERT INTO img VALUES (?, ?, ?, ?, ?, ?)"""
-                          ("""avatar{num}""", 'avatar', 'png', f"""img/avatar{num}.png""", 240, 240))
+            cursor.execute("""INSERT INTO img VALUES (?, ?, ?, ?, ?, ?)""",
+                          (f"""avatar{num}""", 'avatar', 'png', f"""img/avatar{num}.png""", 240, 240))
             database.commit()
         except KeyboardInterrupt:
             break
@@ -79,6 +80,31 @@ def get_img():
         else:
             print("Invalid choice")
 
+def init():
+    os.chdir("img")
+    if len(sys.argv) != 2:
+        print("""No options selected
+"get_img.py --help" for help""")
+        exit()
+    else:
+        if sys.argv[1] == "--help":
+            print("""Usage: get_img.py <options>
+Options: 
+	--acg       : Get good images
+	--avatar    : Get avatars
+	--help      : Show this message
+	--wallpaper : Get wallpaper""")
+        elif sys.argv[1] == "--acg":
+            get_acg()
+        elif sys.argv[1] == "--avatar":
+            get_avatar()
+        elif sys.argv[1] == "--wallpaper":
+            get_wallpaper()
+        else:
+            print("""Invalid choice
+"get_img.py --help" for help""")
+        
+        
 
 if __name__ == "__main__":
-    get_img()
+    init()
